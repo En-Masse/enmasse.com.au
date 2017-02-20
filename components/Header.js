244e66973e2ce65217c6a5049024638d23d2bc9a
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2017-02-20 15:21:11 +1100

    Starting on the header component with css to match

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,28 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {prefixLink} from 'gatsby-helpers'
 import {Link} from 'react-router'
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.props.pages.map(page => {
-            if (!page.path.includes('blog/') && page.path !== '/404/' && page.path !== '/') {
-              return (
-                <li>
-                  <Link style={{
-                    boxShadow: 'none'
-                  }} to={prefixLink(page.path)}>{page.data.title}</Link>
-                </li>
-              )
-            }
-          })}
-        </ul>
-      </div>
-    )
-  }
+const Header = (props) => {
+  const {pages} = props
+  return (
+    <header className="container">
+      <Link to={prefixLink('/')} className="six columns">
+        <h1>En Masse</h1>
+      </Link>
+      {pages.map((page) => {
+        if (!page.path.includes('blog/') && page.path !== '/404/' && page.path !== '/') {
+          return (
+            <Link key={page.path} className="one column" to={prefixLink(page.path)}>{page.data.title}</Link>
+          )
+        }
+        return false
+      })}
+    </header>
+  )
 }
 
 Header.propTypes = {
-  route: React.PropTypes.object
+  pages: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
 }
 
 export default Header
