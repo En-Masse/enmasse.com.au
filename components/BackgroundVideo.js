d4e1ac565c47e3e614cc24829e4c448fce5cfedd
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2017-02-23 10:01:21 +1100

    🔧 Simplified the background video component and made it stateless

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,74 +1,58 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 import {prefixLink} from 'gatsby-helpers'
 
 import './BackgroundVideo.scss'
 
-class BackgroundVideo extends Component {
-  static propTypes = {
-    videos: React.PropTypes.arrayOf(PropTypes.shape({
-      src: PropTypes.string,
-      type: PropTypes.string,
-    })),
-    children: PropTypes.node,
-    preload: PropTypes.bool,
-    loop: PropTypes.bool,
-    autoPlay: PropTypes.bool,
-    className: PropTypes.string,
-    overlayClassName: PropTypes.string,
-    videoId: PropTypes.string,
-    overlay: PropTypes.bool,
-    poster: PropTypes.string,
-    onClick: PropTypes.func,
+const BackgroundVideo = ({
+  videos,
+  videoId,
+  preload,
+  loop,
+  autoPlay,
+  poster,
+}) => {
+  const videoProps = {
+    preload,
+    loop,
+    autoPlay,
+    poster,
   }
+  const videoElements = videos.map(video =>
+    <source key={video.src} src={video.src} type={video.type} />,
+  )
+  return (
+    <div className="videoContainer">
+      <video id={videoId} className="video-background" {...videoProps}>
+        {videoElements}
+      </video>
+    </div>
+  )
+}
 
-  static defaultProps = {
-    videos: [
-      {
-        src: prefixLink('/static/video/handshake.mov'),
-        type: 'video/mp4',
-      },
-    ],
-    preload: true,
-    loop: true,
-    autoPlay: true,
-    className: '',
-    overlayClassName: '',
-    videoId: 'video',
-    overlay: true,
-  }
+BackgroundVideo.propTypes = {
+  videos: React.PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+    type: PropTypes.string,
+  })),
+  loop: PropTypes.bool,
+  autoPlay: PropTypes.bool,
+  videoId: PropTypes.string,
+  preload: PropTypes.bool,
+  poster: PropTypes.string,
+}
 
-  componentWillUnmount() {
-    const videoElement = document.getElementById(this.props.videoId)
-    if (videoElement) {
-      videoElement.pause()
-      videoElement.src = ''
-      videoElement.load()
-    }
-  }
-
-  render() {
-    const videos = this.props.videos.map((video, index) =>
-      <source key={index} src={video.src} type={video.type} />)
-
-    const videoProps = {
-      preload: this.props.preload,
-      loop: this.props.loop,
-      autoPlay: this.props.autoPlay,
-      poster: this.props.poster,
-    }
-
-    const overlayElement = (this.props.overlay) ? <div className="videoOverlay" /> : null
-
-    return (
-      <div className="videoContainer" onClick={this.props.onClick}>
-        <video id={this.props.videoId} className="video-background" {...videoProps}>
-          {videos}
-        </video>
-        {overlayElement}
-        <div className="video-content">{this.props.children}</div>
-      </div>
-    )
-  }
+BackgroundVideo.defaultProps = {
+  videos: [
+    {
+      src: prefixLink('/static/video/handshake.mov'),
+      type: 'video/mp4',
+    },
+  ],
+  preload: true,
+  loop: true,
+  autoPlay: true,
+  videoId: 'video',
+  poster: '',
 }
 
 export default BackgroundVideo
